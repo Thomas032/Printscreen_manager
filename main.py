0c9c1c5928eccea7e95bb7ab98408f65bb56cd39
Thomas032 <80817250+Thomas032@users.noreply.github.com>
2021-08-04 15:11:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,110 +1,115 @@
-#COPIRIGHT THOMAS032 -- 2021
 from tkinter import *
+import os
 import keyboard
 import pyautogui
-import os
-import datetime 
+import datetime
 
-def gui():
-    m = Tk()
-    m.title("printscreen manager") 
-    w = 450
-    h = 50
-    x = 880
-    y = 10
-    m.geometry('%dx%d+%d+%d' % (w, h, x, y))
-    label = Label(m, text="Název obrázku <ENTER>")
-    name = Entry(m)
-    label.config(font=("Helvetica", 15))
-    name.config(width= 15, font=("Courier", 15))
-    name.grid(row=0, column=1)
-    label.grid(row=0)
+class Gui_entry():
     
-    def callback(event):
-        if not "\\" in name.get():
-            with open("entry.txt", "w+") as file:
-                file.truncate()
-                file.write(name.get())
-                file.close()
-        if '.' in name.get():
-            with open("entry.txt", "w+") as file:
-                file.truncate()
-                data = name.get()
-                correct = data.split('.')
-                correct = correct[0]
-                file.write(correct)
-                file.close()
-        else: 
-            with open("entry.txt", "w+") as file:
-                file.truncate()
-                data = name.get()
-                correct = data.replace("\\", "")
-                file.write(correct)
-                file.close()
+    def __init__(self, master):
+        self.master = master
+        master.title("Printscreen manager")
+        self.width , self.height = (450,50)
+        self.x, self.y = (880, 10)
+        master.geometry('%dx%d+%d+%d'%(self.width, self.height, self.x, self.y))
+        self.label = Label(master, text="Název obrázku <ENTER>")
+        self.label.config(font=("Helvetica", 15))
+        self.label.grid(row=0)
+        self.file_name = Entry(self.master)
+        self.file_name.config(width= 15, font=("Courier", 15))
+        self.file_name.grid(row=0, column = 1)
+        def callback(event):
+            name = self.file_name.get()
+            if '.' in name:
+                with open("entry.txt", "w+") as file:
+                    file.truncate()
+                    data = name
+                    correct = data.split('.')
+                    correct = correct[0]
+                    file.write(correct)
+                    file.close()
+            if '\\' in name: 
+                with open("entry.txt", "w+") as file:
+                    file.truncate()
+                    data = name
+                    correct = data.replace("\\", "")
+                    file.write(correct)
+                    file.close()
+            else:
+                with open("entry.txt", "w+") as file:
+                    file.truncate()
+                    file.write(name)
+                    file.close()
+            print(f"name you entered: {name}")  
+            self.master.destroy()
             
-        m.destroy()
+        self.master.bind('<Return>', callback)
         
-    
-    m.bind('<Return>', callback)
-    m.mainloop()
+class Gui_exists():
+    def __init__(self, master, file_name, image):
+        global callback
+        self.master = master   
+        self.width, self.height =  (510, 125)
+        self.x, self.y = (500, 200)
+        master.geometry("%dx%d+%d+%d"%(self.width, self.height, self.x, self.y)) 
+        master.title("FILE ALREADY EXISTS!")
+        
+        self.label = Label(master, text=f"File '{file_name}' already exists, do you want to over write it?")
+        self.label.config(font=("Helvetica", 15))
+        
+        self.button_yes = Button(master, text= "YES", command =lambda: callback("p"))
+        self.button_yes.config(font=("Helvetica", 15), fg="green")
+        def n_overwrite():
+            today = datetime.datetime.today()
+            ran = str(file_name)+ str("-"+today.strftime("%d.%m.%Y"))
+            image.save(r"C:\Users\YOUR_PATH\%s.png" %ran)
+            self.master.destroy()
+        self.button_no = Button(master, text= "NO", command = n_overwrite)
+        self.button_no.config(font=("Helvetica", 15), fg="red")
+        
+        self.label.grid(row=0, column=1)
+        self.button_yes.grid(row=1, column=1)
+        self.button_no.grid(row=2, column=1)
+        
+        def callback(event):
+            image.save(r"C:\Users\YOUR_PATH\%s.png" %file_name)
+            self.master.destroy()
+            
+        master.bind("<Return>", callback )
+        
+        
+global read_file
+def read_file():
     with open("entry.txt", "r+") as f:
-                ff = f.read()
-                f.close()
-    return ff
-    
-
-def exists(file_name, image):
-    
-    r = Tk()
-    w = 510
-    h = 125
-    x = 500
-    y = 200
-    r.geometry('%dx%d+%d+%d'% (w ,h ,x ,y))
-    r.title("File already exists!")
-    lab = Label(r, text=f"File '{file_name}' already exists do yu want to overwrite it?")
-    lab.config(font=("Helvetica", 15))
-    def callback(event):
-        image.save(r"C:\Users\YOUR_PATH\Pyscreens\%s.png" %file_name)
-        r.destroy()
-    def overw():
-        image.save(r"C:\Users\YOUR_PATH\Pyscreens\%s.png" %file_name)
-        r.destroy()
+        data = f.read()
         
-        
-    def notverw():
-        today = datetime.datetime.today()
-        ran = str(file_name)+ str("-"+today.strftime("%d.%m.%Y"))
-        image.save(r"C:\Users\YOUR_PATH\Pyscreens\%s.png" %ran)
-        r.destroy()
-        
-    but = Button(r, text="NO", command=notverw)
-    but.config(font=("Helvetica", 15), fg="red")
-    but2 = Button(r, text="YES", command=overw)
-    but2.config(font=("Helvetica", 15), fg="green")
-    
-    lab.grid(row=0, column=1)
-    but.grid(row=2, column=1)
-    but2.grid(row=1, column=1)
-    r.bind('<Return>', callback)
-    r.mainloop()
-    
-    
-  
+        return data
+global clear_file
+def clear_file():
+    with open("entry.txt", "w+") as f:
+        f.truncate()
 def main():
+    
     while True:
         if keyboard.is_pressed("ctrl+print screen"):
-            im1 = pyautogui.screenshot()
-            name = gui()
-            file = name
-            if not os.path.exists(r"C:\Users\YOUR_PATH\%s.png" %file):
-                im1.save(r"C:\Users\YOUR_PATH\%s.png" %file) 
-            else:
-                write = exists(file, im1)
-                if write==True:
-                    im1.save(r"C:\Users\YOUR_PATH\%s.png" %file)
-                if write ==False:
-                    print("do not overwrite")
+            im_1 = pyautogui.screenshot()
+            a_root = Tk()
+            Gui_entry(a_root)
+            a_root.mainloop()
+            file = read_file()
+            clear_file()
+            if file != "":
+                if not os.path.exists(r"C:\Users\YOUR_PATH\%s.png" %file):
+                    im_1.save(r"C:\Users\YOUR_PATH\%s.png" %(file)) 
                     
-
-main()        
+                else:
+                    b_root = Tk()
+                    Gui_exists(b_root, file, im_1)   
+                    b_root.mainloop() 
+            else:
+                pass
+            
+    
+          
+main()
+        
